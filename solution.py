b2cbb211af226c3229d83a080be686fe2ae3bebe
Lilibete <info@lilibete.com>
2017-11-09 11:50:40 -0800

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,16 @@
 assignments = []
 
-def assign_value(values, box, value):
+def assign_value(values, square, value):
     """
     Please use this function to update your values dictionary!
     Assigns a value to a given box. If it updates the board record it.
     """
 
     # Don't waste memory appending actions that don't actually change any values
-    if values[box] == value:
+    if values[square] == value:
         return values
 
-    values[box] = value
+    values[squares] = value
     if len(value) == 1:
         assignments.append(values.copy())
     return values
@@ -29,19 +29,26 @@ def naked_twins(values):
 
 def cross(A, B):
     "Cross product of elements in A and elements in B."
-    pass
+    return [a+b for a in A for b in B]
+
+digits = '123456789'
+rows = 'ABCDEGHI'
+cols = digits
+squares = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF',"GHI') for cs in ('123','456','789')]
+unitlist = row_units + column_units + square_units
+units = dict((s, [u for u in unitlist if s in u])
+             for s in squares)
+peers = dict((s, set(sum(units[s],[]))-set([s]))
+             for s in squares)
 
 def grid_values(grid):
-    """
-    Convert grid into a dict of {square: char} with '123456789' for empties.
-    Args:
-        grid(string) - A grid in string form.
-    Returns:
-        A grid in dictionary form
-            Keys: The boxes, e.g., 'A1'
-            Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
-    """
-    pass
+    "Convert grid into a dict of {square: char} with '123456789' for empties."
+    chars = [c for c in grid if c in digits or c in '.']
+    assert len(chars) == 81
+    return dict(zip(squares, chars))
 
 def display(values):
     """
@@ -49,19 +56,55 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1+max(len(values[s]) for s in squares)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''..join(values[r+c].center(width)+('|' if c in '36' else '')
+                    for c in cols))
+        if r in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+    solved_values = [squares for squares in values.keys() if len(values[squares]) == 1]
+    for squares in solved_values:
+        digit = values[squares]
+        for peer in peers[squares]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
 
 def only_choice(values):
-    pass
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [squares for squares in unit if digit in values[squares]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
 
 def reduce_puzzle(values):
-    pass
+    stalled = False
+    while not stalled:
+        solved_values_before = len([squares for squares in values.keys() if len(values[squares] == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        solved_values_after = len([squares for squares in values.keys() if len(values[squares]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([squares for squares in values.keys() if len(values[squares]) == 0]):
+            return False
+    return values    
 
 def search(values):
-    pass
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in squares):
+        return values ## Solved!
+    n,s = min((len(values[s]), s) for s in squares if len(values[s]) > 1)
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
@@ -85,3 +128,4 @@ if __name__ == '__main__':
         pass
     except:
         print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
+
